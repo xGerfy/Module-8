189add1955f19d3652e7d7f2d237f77b1d21660d
Roman Ponomarev <roman.ponomarev66@gmail.com>
2022-12-01 13:48:13 +0400

    feat: add constructor for class && classes methods

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
 <?php
 
-    
+class TelegraphText
+{
+    public $title, $text, $author, $published, $slug;
+    public function __construct($author, $slug)
+    {
+        $this->author = $author;
+        $this->slug = $slug;
+        $this->published = date('d/m/y/h/i');
+    }
+    public function storeText()
+    {
+        $arrayText['title'] = $this->title;
+        $arrayText['text'] = $this->text;
+        $arrayText['author'] = $this->author;
+        $arrayText['published'] = $this->published;
+        file_put_contents($this->slug, serialize($arrayText));
+    }
+    public function loadText()
+    {
+        if (file_exists($this->slug)) {
+            $arrayText = unserialize(file_get_contents($this->slug));
+            $this->title = $arrayText['title'];
+            $this->text = $arrayText['text'];
+            $this->author = $arrayText['author'];
+            $this->published = $arrayText['published'];
+            return ($this->text);
+        }
+    }
+    public function editText($title, $text)
+    {
+        $this->title = $title;
+        $this->text = $text;
+    }
+}
+
+$text1 = new TelegraphText('Ponomarev', 'test.txt');
+$text1->editText('Hello', 'World');
+$text1->storeText();
+$text1->loadText();
+var_dump($text1);
